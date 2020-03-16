6e1c944ec32dd19549e1b6f44d11831b98873294
Diogo Frazão <doga.frazao@gmail.com>
2020-03-16 16:13:41 +0000

    novos logos

--- routes/web.php
+++ routes/web.php
@@ -27,32 +27,7 @@ Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('/admin/dashboard', function () {
+Route::get('/admin', function () {
     return view('admin.dashboard');
-});
-
-
-Route::get('/admin/students', function () {
-    return view('admin.students');
-});
-
-Route::get('/admin/professors', function () {
-    return view('admin.professors');
-});
-
-Route::get('/admin/subjects', function () {
-    return view('admin.subjects');
-});
-
-Route::get('/admin/classes', function () {
-    return view('admin.classes');
-});
-
-Route::get('/admin/import_data', function () {
-    return view('admin.import_data');
-});
-
-Route::get('/admin/permissions', function () {
-    return view('admin.permissions');
 });
 
