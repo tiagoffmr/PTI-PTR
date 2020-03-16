e4011b52dbe6997b37c86ba860b9c4ad53ed26dc
dre <andre.vsilva98@gmail.com>
2020-03-16 17:54:59 +0000

    horario front end

---
+++ resources/views/horario.blade.php
@@ -0,0 +1,178 @@
+@extends('layout.base')
+@section('content')
+    <div class="container full-height">
+        {{--        <div class="container p-4">
+                    <div class="row ">
+                        <div class="col-sm-1" style="border: solid 1px black;"></div>
+                        <div class="col-sm-1" style="border: solid 1px black;">Segunda-Feira</div>
+                        <div class="col-sm-1" style="border: solid 1px black;">Terça-Feira</div>
+                        <div class="col-sm-1" style="border: solid 1px black;">Quarta-Feira</div>
+                        <div class="col-sm-1" style="border: solid 1px black;">Quinta-Feira</div>
+                        <div class="col-sm-1" style="border: solid 1px black;">Sexta-Feira</div>
+                        <div class="col-sm-1" style="border: solid 1px black;">Sábado</div>
+                        <div class="col-sm-1" style="border: solid 1px black;">Domingo</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-1" style="border: solid 1px black;">8:00h-9:00h</div>
+                        <div class="col-sm-1" style="border: solid 1px black;"></div>
+                        <div class="col-sm-1" style="border: solid 1px black;"></div>
+                        <div class="col-sm-1" style="border: solid 1px black;"></div>
+                        <div class="col-sm-1" style="border: solid 1px black;"></div>
+                        <div class="col-sm-1" style="border: solid 1px black;"></div>
+                        <div class="col-sm-1" style="border: solid 1px black;"></div>
+                        <div class="col-sm-1" style="border: solid 1px black;"></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-1" style="border: solid 1px black;">10:00h-11:00h</div>
+                        <div class="col-sm-1" style="border: solid 1px black;"></div>
+                        <div class="col-sm-1" style="border: solid 1px black;"></div>
+                        <div class="col-sm-1" style="border: solid 1px black;"></div>
+                        <div class="col-sm-1" style="border: solid 1px black;"></div>
+                        <div class="col-sm-1" style="border: solid 1px black;"></div>
+                        <div class="col-sm-1" style="border: solid 1px black;"></div>
+                        <div class="col-sm-1" style="border: solid 1px black;"></div>
+                    </div>--}}
+
+
+        {{--        </div>--}}
+
+        <div class="grid-container">
+            <div></div>
+            <div>Segunda</div>
+            <div>Terça</div>
+            <div>Quarta</div>
+            <div>Quinta</div>
+            <div>Sexta</div>
+            <div>Sábado</div>
+            <div>Domingo</div>
+            <div style="border: 1px solid black;">8:00h-9:00h</div>
+            <div style="border-top: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-top: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-top: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-top: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-top: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-top: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-top: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-left: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black;">9:00h-10:00h</div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-left: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black;">10:00h-11:00h</div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-left: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black;">11:00h-12:00h</div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-left: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black;">12:00h-13:00h</div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-left: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black;">13:00h-14:00h</div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-left: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black;">14:00h-15:00h</div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-left: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black;">15:00h-16:00h</div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-left: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black;">16:00h-17:00h</div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-left: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black;">17:00h-18:00h</div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div style="border-bottom: 1px solid black; border-right: 1px solid black;"></div>
+            <div></div>
+            <div>André
+                <span class="border bg-primary align-middle "></span>
+            </div>
+            <div>António
+                <span class="border bg-success align-middle "></span>
+            </div>
+            <div>Diogo
+                <span class="border bg-info align-middle "></span>
+            </div>
+            <div>Guilherme
+                <span class="border bg-warning align-middle "></span>
+            </div>
+            <div>João
+                <span class="border bg-danger align-middle "></span>
+            </div>
+            <div>Tiago
+                <span class="border bg-secondary align-middle "></span>
+            </div>
+            <div>Vasco
+                <span class="border bg-dark align-middle "></span>
+            </div>
+        </div>
+
+
+
+        </div>
+
+        <style>
+            .border {
+                display: inline-block;
+                width: 10px;
+                height: 10px;
+            }
+
+            .grid-container {
+                display: grid;
+                grid-template-columns: auto auto auto auto auto auto auto auto;
+                padding: 10px;
+            }
+
+            .grid-container > div {
+                padding-top: 15px;
+                padding-bottom: 15px;
+                text-align: center;
+                font-size: 10px;
+            }
+        </style>
+
+    </div>
+@endsection
