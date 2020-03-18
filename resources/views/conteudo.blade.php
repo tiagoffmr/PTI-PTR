ba01b06a6ea2483aa59f07e05ba5b1a302344707
Tiago <tiagorobalo1999@gmail.com>
2020-03-18 18:44:12 +0000

    pagina conteudo alterada, falta mudar NOTAS

--- resources/views/conteudo.blade.php
+++ resources/views/conteudo.blade.php
@@ -3,29 +3,24 @@
 
 <div class="container w-75 h-75" >
     <h3 class="pt-3">Css - Projeto 1</h3>
-    <div class="row h-100 rounded" style="background-color: #ededed;">
-        <div class="col mt-2 h-100" style="background-color: #c6c6c6;">
-            <div class="container-fluid rounded d-flex flex-row mt-3" >
-                <i class="fas fa-folder fa-4x" style="color: #ffce52;"></i>
-                <span>proj1-v1.zip</span>
-                <i class="fas fa-folder fa-4x" style="color: #ffce52;"></i>
-                <span>proj1-v2.zip</span>
-                <i class="fas fa-folder fa-4x" style="color: #ffce52;"></i>
-                <span>proj2-v1.zip</span>
-            </div>
-            <div class="container-fluid clearfix" style="background-color: #c6c6c6;">
-                <button type="submit" class="btn btn-primary float-right">Submeter</button>
+    <div class="row h-100 " style="background-color: #ededed;">
+        <div class="col mt-3 ml-3 rounded" style="background-color: #c6c6c6; height: 87%; position: relative;">
+            <div class="container-fluid d-flex flex-row mt-3" >
+                <div class="container text-center"><i class="fas fa-folder fa-4x" style="color: #ffce52;"></i>proj1-v1.zip</div>
+                <div class="container text-center"><i class="fas fa-folder fa-4x" style="color: #ffce52;"></i>proj1-v2.zip</div>
+                <div class="container text-center"><i class="fas fa-folder fa-4x" style="color: #ffce52;"></i>proj2-v1.zip</div>
+                <button type="submit" class="btn btn-sm mb-2 mr-2" style="background: #2c3fb1; color: white; position: absolute; bottom: 0px; right: 0px;">Submeter</button>
             </div>
         </div>
-        <div class="col-3 mt-2" style="height: 95%;">
-            <div class="container-fluid rounded h-100" style="background-color: #c6c6c6;">
+        <div class="col-3 mt-3 rounded" style="height: 87%;">
+            <div class="container-fluid rounded h-100 text-center pt-2" style="background-color: #c6c6c6;">
                 <h5>Documentação </h5>
                 <p> Enunciado </p>
             </div>
         </div>
         <div class="col-4 h-100">
-            <div class="row mt-2" style="height: 50%;">
-                <div class="container-fluid rounded" style="background-color: #ffce52;">
+            <div class="row mt-3 mr-1" style="height: 60%;">
+                <div class="container-fluid rounded pt-2" style="background-color: #ffce52;">
                     <h5> Notas </h5>
                     <div class="custom-control custom-checkbox">
                         <input type="checkbox" class="custom-control-input" id="defaultChecked" checked="">
@@ -41,16 +36,16 @@
                     </div>
                 </div>
             </div>
-            <div class="row mt-2" style="height: 45%;">
-                <div class="container-fluid rounded" style="background-color: #c6c6c6;">
+            <div class="row mt-2 mr-1" style="height: 25%;">
+                <div class="container-fluid rounded text-center pt-2" style="background-color: #c6c6c6; position: relative;">
                     <h5>Reunião</h5>
                     <div>24/12/2020 15:00 </div>
-                    <button type="submit" class="btn btn-primary float-right">Agendar</button>
+                    <button type="submit" class="btn btn-sm mr-2 mb-2" style="background: #2c3fb1; color: white; position: absolute; bottom: 0px; right: 0px;">Agendar</button>
                 </div>
             </div>
-            <button type="submit" class="btn btn-primary float-right">Sair do Grupo</button>
+            <button type="submit" class="btn btn-sm float-right mt-2 mr-1" style="background: #2c3fb1; color: white;">Sair do Grupo</button>
         </div>
-    </div> 
+    </div>
 </div>
 @endsection
 
