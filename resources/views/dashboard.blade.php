f9ef09d8c32bae571fd9fcbf7d168348f00b4b5f
Diogo Frazao <doga.frazao@gmail.com>
2020-03-24 00:16:37 +0000

    upadate dashboard c/ calendario inicial

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,12 +3,141 @@
 <div class="container-xl">
     <div class="row mt-2" style="height: 90vh;">
 
-        <div class="col-sm-3">
+        <div class="col-sm-4">
             <div class="container bg-light rounded h-100">
+                <button type="button" class="btn btn-default btn-lg" style="text-align: center;width: 100%;">
+                    <span class="fas fa-chevron-up"></span>
+                </button>
+                <h3 style="text-align: center;">March 2020</h3>
+                <table class="table  table-sm" style="text-align: center;">
+                    <thead>
+                    <tr>
+                        <th scope="col">Seg</th>
+                        <th scope="col">Ter</th>
+                        <th scope="col">Qua</th>
+                        <th scope="col">Qui</th>
+                        <th scope="col">Sex</th>
+                        <th scope="col">Sáb</th>
+                        <th scope="col">Dom</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>5</td>
+                        <td>6</td>
+                        <td>7</td>
+                    </tr>
+                    <tr>
+                        <td>8</td>
+                        <td>9</td>
+                        <td>10</td>
+                        <td>11</td>
+                        <td>12</td>
+                        <td>13</td>
+                        <td>14</td>
+                    </tr>
+                    <tr>
+                        <td>15</td>
+                        <td>16</td>
+                        <td>17</td>
+                        <td>18</td>
+                        <td>19</td>
+                        <td>20</td>
+                        <td>21</td>
+                    </tr>
+                    <tr>
+                        <td>22</td>
+                        <td>23</td>
+                        <td>24</td>
+                        <td>25</td>
+                        <td>26</td>
+                        <td>26</td>
+                        <td>27</td>
+                    </tr>
+                    <tr>
+                        <td>28</td>
+                        <td>29</td>
+                        <td>30</td>
+                        <td>31</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    </tbody>
+                </table>
+
+                <h3 style="text-align: center;">March 2020</h3>
+                <table class="table  table-sm" style="text-align: center;width: 100%;">
+                    <thead>
+                    <tr>
+                        <th scope="col">Seg</th>
+                        <th scope="col">Ter</th>
+                        <th scope="col">Qua</th>
+                        <th scope="col">Qui</th>
+                        <th scope="col">Sex</th>
+                        <th scope="col">Sáb</th>
+                        <th scope="col">Dom</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>5</td>
+                        <td>6</td>
+                        <td>7</td>
+                    </tr>
+                    <tr>
+                        <td>8</td>
+                        <td>9</td>
+                        <td>10</td>
+                        <td>11</td>
+                        <td>12</td>
+                        <td>13</td>
+                        <td>14</td>
+                    </tr>
+                    <tr>
+                        <td>15</td>
+                        <td>16</td>
+                        <td>17</td>
+                        <td>18</td>
+                        <td>19</td>
+                        <td>20</td>
+                        <td>21</td>
+                    </tr>
+                    <tr>
+                        <td>22</td>
+                        <td>23</td>
+                        <td>24</td>
+                        <td>25</td>
+                        <td>26</td>
+                        <td>26</td>
+                        <td>27</td>
+                    </tr>
+                    <tr>
+                        <td>28</td>
+                        <td>29</td>
+                        <td>30</td>
+                        <td>31</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    </tbody>
+                </table>
+                <button type="button" class="btn btn-default btn-lg" style="text-align: center;width: 100%;">
+                    <span class="fas fa-chevron-down"></span>
+                </button>
             </div>
         </div>
 
-        <div class="col-sm-9">
+        <div class="col-sm-8">
             <div class="overflow-auto bg-light rounded h-100">
                 <h3 class="pt-3 pl-3">Dashboard</h3>
                 <div class="container overflow-auto mw-80" style="max-height: 75vh;">
@@ -20,7 +149,8 @@
                         </button>
                     </div>
 
-                    <div class="container overflow-auto bg-secondary p-2 rounded d-none" id="css-groups">
+                    <div class="container overflow-hidden bg-secondary  rounded-bottom doff" id="css-groups" style="display: none;">
+                        <p></p>
                         <p>Exemplo</p>
                         <p>Lalalala</p>
                         <p>Lalalala</p>
@@ -35,7 +165,8 @@
                         </button>
                     </div>
 
-                    <div class="container overflow-auto bg-secondary p-2 rounded d-none" id="pgp-groups">
+                    <div class="container overflow-hidden bg-secondary rounded-bottom doff" id="pgp-groups" style="display: none;">
+                        <p></p>
                         <p>Exemplo</p>
                         <p>Lalalala</p>
                         <p>Lalalala</p>
@@ -50,7 +181,8 @@
                         </button>
                     </div>
 
-                    <div class="container overflow-auto bg-secondary p-2 rounded d-none" id="bd-groups">
+                    <div class="container overflow-hidden bg-secondary rounded-bottom doff" id="bd-groups" style="display: none;">
+                        <p></p>
                         <p>Exemplo</p>
                         <p>Lalalala</p>
                         <p>Lalalala</p>
@@ -69,15 +201,19 @@
 
             $(".cadeira").click(function(){
                 id = $(this).attr('id');
-                if ($("#" + id + "-groups").hasClass( "d-none" )) {
-                    $("#" + id + "-groups").removeClass( "d-none" );
+                if ($("#" + id + "-groups").hasClass( "doff" )) {
+                    $("#" + id + "-groups").removeClass( "doff" );
                     $("#" + id).children().addClass('fa-minus').removeClass('fa-plus');
+                    $("#" + id + "-groups").slideDown(400);
                 } else {
-                    $("#" + id + "-groups").addClass( "d-none" );
+                    $("#" + id + "-groups").addClass( "doff" );
                     $("#" + id).children().addClass('fa-plus').removeClass('fa-minus');
+                    $("#" + id + "-groups").slideUp(400);
                 }
             });
         });
+
+
     </script>
 @endsection
 
