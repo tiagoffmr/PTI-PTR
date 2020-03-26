034b692739fbb5806ed39fb14fe79107773871a8
Diogo Frazao <doga.frazao@gmail.com>
2020-03-26 23:26:28 +0000

    add moment.js na head e calendario funcional na dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,133 +5,155 @@
 
         <div class="col-sm-4">
             <div class="container bg-light rounded h-100">
-                <button type="button" class="btn btn-default btn-lg" style="text-align: center;width: 100%;">
+                <button type="button" class="previous btn btn-default btn-lg" style="text-align: center;width: 100%;">
                     <span class="fas fa-chevron-up"></span>
                 </button>
-                <h3 style="text-align: center;">March 2020</h3>
-                <table class="table  table-sm" style="text-align: center;">
-                    <thead>
-                    <tr>
-                        <th scope="col">Seg</th>
-                        <th scope="col">Ter</th>
-                        <th scope="col">Qua</th>
-                        <th scope="col">Qui</th>
-                        <th scope="col">Sex</th>
-                        <th scope="col">Sáb</th>
-                        <th scope="col">Dom</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
-                        <td>7</td>
-                    </tr>
-                    <tr>
-                        <td>8</td>
-                        <td>9</td>
-                        <td>10</td>
-                        <td>11</td>
-                        <td>12</td>
-                        <td>13</td>
-                        <td>14</td>
-                    </tr>
-                    <tr>
-                        <td>15</td>
-                        <td>16</td>
-                        <td>17</td>
-                        <td>18</td>
-                        <td>19</td>
-                        <td>20</td>
-                        <td>21</td>
-                    </tr>
-                    <tr>
-                        <td>22</td>
-                        <td>23</td>
-                        <td>24</td>
-                        <td>25</td>
-                        <td>26</td>
-                        <td>26</td>
-                        <td>27</td>
-                    </tr>
-                    <tr>
-                        <td>28</td>
-                        <td>29</td>
-                        <td>30</td>
-                        <td>31</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    </tbody>
-                </table>
+                <h3 class="month1" style="text-align: center;"></h3>
+                <div class="table-responsive-xl">
+                    <table class="month1-cal table table-sm table-borderless" style="text-align: center;">
+                        <thead>
+                        <tr>
+                            <th scope="col">Seg</th>
+                            <th scope="col">Ter</th>
+                            <th scope="col">Qua</th>
+                            <th scope="col">Qui</th>
+                            <th scope="col">Sex</th>
+                            <th scope="col">Sáb</th>
+                            <th scope="col">Dom</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr class="week1">
+                            <td class="1"></td>
+                            <td class="2"></td>
+                            <td class="3"></td>
+                            <td class="4"></td>
+                            <td class="5"></td>
+                            <td class="6"></td>
+                            <td class="7"></td>
+                        </tr>
+                        <tr class="week2">
+                            <td class="1"></td>
+                            <td class="2"></td>
+                            <td class="3"></td>
+                            <td class="4"></td>
+                            <td class="5"></td>
+                            <td class="6"></td>
+                            <td class="7"></td>
+                        </tr>
+                        <tr class="week3">
+                            <td class="1"></td>
+                            <td class="2"></td>
+                            <td class="3"></td>
+                            <td class="4"></td>
+                            <td class="5"></td>
+                            <td class="6"></td>
+                            <td class="7"></td>
+                        </tr>
+                        <tr class="week4">
+                            <td class="1"></td>
+                            <td class="2"></td>
+                            <td class="3"></td>
+                            <td class="4"></td>
+                            <td class="5"></td>
+                            <td class="6"></td>
+                            <td class="7"></td>
+                        </tr>
+                        <tr class="week5">
+                            <td class="1"></td>
+                            <td class="2"></td>
+                            <td class="3"></td>
+                            <td class="4"></td>
+                            <td class="5"></td>
+                            <td class="6"></td>
+                            <td class="7"></td>
+                        </tr>
+                        <tr class="week6">
+                            <td class="1"></td>
+                            <td class="2"></td>
+                            <td class="3"></td>
+                            <td class="4"></td>
+                            <td class="5"></td>
+                            <td class="6"></td>
+                            <td class="7"></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
 
-                <h3 style="text-align: center;">March 2020</h3>
-                <table class="table  table-sm" style="text-align: center;width: 100%;">
-                    <thead>
-                    <tr>
-                        <th scope="col">Seg</th>
-                        <th scope="col">Ter</th>
-                        <th scope="col">Qua</th>
-                        <th scope="col">Qui</th>
-                        <th scope="col">Sex</th>
-                        <th scope="col">Sáb</th>
-                        <th scope="col">Dom</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
-                        <td>7</td>
-                    </tr>
-                    <tr>
-                        <td>8</td>
-                        <td>9</td>
-                        <td>10</td>
-                        <td>11</td>
-                        <td>12</td>
-                        <td>13</td>
-                        <td>14</td>
-                    </tr>
-                    <tr>
-                        <td>15</td>
-                        <td>16</td>
-                        <td>17</td>
-                        <td>18</td>
-                        <td>19</td>
-                        <td>20</td>
-                        <td>21</td>
-                    </tr>
-                    <tr>
-                        <td>22</td>
-                        <td>23</td>
-                        <td>24</td>
-                        <td>25</td>
-                        <td>26</td>
-                        <td>26</td>
-                        <td>27</td>
-                    </tr>
-                    <tr>
-                        <td>28</td>
-                        <td>29</td>
-                        <td>30</td>
-                        <td>31</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    </tbody>
-                </table>
-                <button type="button" class="btn btn-default btn-lg" style="text-align: center;width: 100%;">
+                <h3 class="month2" style="text-align: center;"></h3>
+                <div class="table-responsive-xl">
+                    <table class="month2-cal table table-sm table-borderless" style="text-align: center;wclassth: 100%;">
+                        <thead>
+                        <tr>
+                            <th scope="col">Seg</th>
+                            <th scope="col">Ter</th>
+                            <th scope="col">Qua</th>
+                            <th scope="col">Qui</th>
+                            <th scope="col">Sex</th>
+                            <th scope="col">Sáb</th>
+                            <th scope="col">Dom</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr class="week1">
+                            <td class="1"></td>
+                            <td class="2"></td>
+                            <td class="3"></td>
+                            <td class="4"></td>
+                            <td class="5"></td>
+                            <td class="6"></td>
+                            <td class="7"></td>
+                        </tr>
+                        <tr class="week2">
+                            <td class="1"></td>
+                            <td class="2"></td>
+                            <td class="3"></td>
+                            <td class="4"></td>
+                            <td class="5"></td>
+                            <td class="6"></td>
+                            <td class="7"></td>
+                        </tr>
+                        <tr class="week3">
+                            <td class="1"></td>
+                            <td class="2"></td>
+                            <td class="3"></td>
+                            <td class="4"></td>
+                            <td class="5"></td>
+                            <td class="6"></td>
+                            <td class="7"></td>
+                        </tr>
+                        <tr class="week4">
+                            <td class="1"></td>
+                            <td class="2"></td>
+                            <td class="3"></td>
+                            <td class="4"></td>
+                            <td class="5"></td>
+                            <td class="6"></td>
+                            <td class="7"></td>
+                        </tr>
+                        <tr class="week5">
+                            <td class="1"></td>
+                            <td class="2"></td>
+                            <td class="3"></td>
+                            <td class="4"></td>
+                            <td class="5"></td>
+                            <td class="6"></td>
+                            <td class="7"></td>
+                        </tr>
+                        <tr class="week6">
+                            <td class="1"></td>
+                            <td class="2"></td>
+                            <td class="3"></td>
+                            <td class="4"></td>
+                            <td class="5"></td>
+                            <td class="6"></td>
+                            <td class="7"></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <button type="button" class="next btn btn-default btn-lg" style="text-align: center;width: 100%;">
                     <span class="fas fa-chevron-down"></span>
                 </button>
             </div>
@@ -213,7 +235,107 @@
             });
         });
 
+        class Calendar {
+
+            constructor() {
+                this.monthDiv = document.querySelector('.month1');
+                this.nextMonthDiv = document.querySelector('.month2');
+                this.prevDiv = document.querySelector('.previous');
+                this.nextDiv = document.querySelector('.next');
+            }
+
+            init() {
+                this.month = [];
+                this.month.push(moment(), moment().add(1, 'months'));
+
+                this.today = moment().startOf('day').format('YYYY-MM-DD');
+
+                this.nextDiv.addEventListener('click', _ => {this.addMonth();});
+                this.prevDiv.addEventListener('click', _ => {this.removeMonth();});
+
+                this.update();
+            }
+
+            update() {
+                this.monthString = this.month[0].clone().format('MMMM YYYY');
+                this.nextMonthString = this.month[1].clone().format('MMMM YYYY');
+
+                this.calendarDays = [];
+                this.calendarDays.push({
+                    first: this.month[0].clone().startOf('month'),
+                    last: this.month[0].clone().endOf('month').date() },
+                    {
+                    first: this.month[1].clone().startOf('month'),
+                    last: this.month[1].clone().endOf('month').date() });
+
+                this.draw();
+            }
+
+            addMonth() {
+                this.month[0].add(1, 'month');
+                this.month[1].add(1, 'month');
+                this.update();
+            }
+
+            removeMonth() {
+                this.month[0].subtract(1, 'month');
+                this.month[1].subtract(1, 'month');
+                this.update();
+            }
+
+            draw() {
+                $('table.month1-cal tr > td').empty()
+                $('table.month2-cal tr > td').empty()
+                $("table.month1-cal tr > td").removeClass("today");
+                $("table.month2-cal tr > td").removeClass("today");
+                this.monthDiv.innerText = this.monthString;
+                this.nextMonthDiv.innerText = this.nextMonthString;
+
+                for (let months = 0; months <= 1; months++) {
+                    let week = 1;
+                    let weekday = moment(this.calendarDays[months].first).isoWeekday();
+                    let start = moment(this.calendarDays[months].first).format('YYYY-MM-DD');
+                    for (let index = 1; index <= this.calendarDays[months].last; index++) {
+                        if (weekday == 8) {
+                            weekday = 1;
+                            week++;
+                        }
+                        $(".month"+[months+1]+"-cal .week"+week+" ."+weekday).text(index);
+                        $(".month"+[months+1]+"-cal .week"+week+" ."+weekday).attr('id',start);
+                        if (start == this.today) {
+                            $(".month"+[months+1]+"-cal .week"+week+" ."+weekday).addClass("today");
+                        }
+                        start = moment(start).add(1, 'day').format('YYYY-MM-DD');
+                        weekday++;
+                    }
+                }
+            }
+        }
+
+        const cal = new Calendar();
+        cal.init();
 
     </script>
+    <style>
+        table {
+            table-layout: fixed;
+        }
+        tr {
+            line-height: 37px;
+            min-height: 37px;
+            height: 37px;
+        }
+        .today  {
+            font-weight: 800;
+            color: #2c3fb1;
+        }
+
+        .selected {
+            background-color: #2196f3;
+            box-shadow: 0 5px 10px -5px rgba(0, 0, 0, 0.75);
+            border-radius: 50%;
+            color: #111;
+        }
+    </style>
 @endsection
 
--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -10,7 +10,7 @@
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.1.0/css/flag-icon.min.css" rel="stylesheet">
 	<link href="/fontawesome-pro-5.12.0-web/css/all.css" rel="stylesheet">
-
+	<script src='https://momentjs.com/downloads/moment-with-locales.js'></script><script  src="./script.js"></script>
 
 </head>
 
